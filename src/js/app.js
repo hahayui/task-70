035065b62692e4ac7fc99719092b1340eb3d627d
Elian Kurtev <eliankurtev@gmail.com>
2022-12-27 16:37:41 +0200

    adding image file, updating the js file in order to scale the imag

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,11 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+const sampleImage = document.querySelector(".image");
+sampleImage.addEventListener("click", () => {
+sampleImage.style.setProperty("scale",2);
+});
+
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
